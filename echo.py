c3393c32b01bb5ad6639076c598b592c24b2c266
jrenning <jack-renning@uiowa.edu>
2024-01-23 18:25:25 -0600

    Initial commit w/ echo

---
+++ echo.py
@@ -0,0 +1,18 @@
+
+def echo(text:str, repetitions: int = 3) -> str:
+    """Imitate a real world echo"""
+    
+    echo = ""
+    for rep in range(repetitions):
+        echo += text[-3+rep:] + "\n"
+    
+    echo += "."
+    
+    return echo
+        
+        
+    
+if __name__ == "__main__":
+    text = input('Yell something at a mountain: ')
+    print(echo(text))
+    
